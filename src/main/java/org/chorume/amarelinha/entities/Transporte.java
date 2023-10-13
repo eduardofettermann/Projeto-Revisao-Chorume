2603a95f7e7d45765b46f730841e2845cf8ebce4
ebonatto <ebonatto.dev@gmail.com>
2023-10-13 15:34:29 -0300

    feat: adiciona atributo carga e método somaPesoCarga

--- src/main/java/org/chorume/amarelinha/entities/Transporte.java
+++ src/main/java/org/chorume/amarelinha/entities/Transporte.java
@@ -1,4 +1,13 @@
 package org.chorume.amarelinha.entities;
 
 public class Transporte {
+    public HashMap<Produto, Integer> carga = new HashMap<>(); // <Produto, Quantidade>
+
+    public double somaPesoCarga(HashMap<Produto, Integer> carga) {
+        double total = 0.0;
+        for (Map.Entry<Produto,Integer> produto : carga.entrySet()) {
+            total += produto.getKey().getPeso() * produto.getValue();
+        }
+        return total;
+    }
 }
