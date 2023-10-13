a26dd9dbe2e9c6184d44b1d81552d793d21f7f7b
Davs <davidsilvateixeira5@hotmail.com>
2023-10-13 15:36:38 -0300

    feat: classe Caminhão com construtor

--- src/main/java/org/chorume/amarelinha/entities/Caminhao.java
+++ src/main/java/org/chorume/amarelinha/entities/Caminhao.java
@@ -1,4 +1,14 @@
 package org.chorume.amarelinha.entities;
 
 public class Caminhao {
+    private String classificacao;  // Pequeno, Médio, Grande
+    private double precoPorKm;     // Preço por quilômetro (R$/km)
+    private double capacidadeMaxima; // Capacidade máxima em toneladas
+
+    // Construtor
+    public Caminhao(String classificacao, double precoPorKm, double capacidadeMaxima) {
+        this.classificacao = classificacao;
+        this.precoPorKm = precoPorKm;
+        this.capacidadeMaxima = capacidadeMaxima;
+    }
 }
