2d9e54968eb585b4114b21d681fb2510d64dbc17
Giovane Antunes Boaro <96142962+GiovaneBoaro@users.noreply.github.com>
2023-10-13 20:56:55 -0300

    feat: adiciona o controle de fluxo do menu

--- src/main/java/org/chorume/amarelinha/entities/CadastroTransporte.java
+++ src/main/java/org/chorume/amarelinha/entities/CadastroTransporte.java
@@ -19,6 +19,33 @@ public class CadastroTransporte {
             System.out.println("6 - Calcular custo total");
             System.out.println("7 - Sair");
             int opcao = scanner.nextInt();
+
+            //determinar qual ação realizar
+            switch(opcao) {
+                case 1 -> {
+                    System.out.println("Função para adicionar caminhão");
+                    adicionarCaminhao();
+                } case 2 -> {
+                    System.out.println("Calcular distância total");
+                    calcularDistanciaTotal();
+                } case 3 -> {
+                    System.out.println("Divisão de cargas entre caminhões");
+                    divideCarga();
+                } case 4 -> {
+                    System.out.println("Verifica produtos");
+                    carregaMaisPesado();
+                } case 5 {
+                    System.out.println("Somar peso das cargas");
+                    somaPesoCarga();
+                } case 6 -> {
+                    System.out.println("Calcular custo total");
+                    calcularCustoTotal();
+                } case 6 -> {
+                    System.out.println("Fechando programa...");
+                    i = false;
+                } default ->
+                        System.out.println("Essa opção não existe! Por favor, digite uma opção válida!");
+            }
         }
     }
 }
