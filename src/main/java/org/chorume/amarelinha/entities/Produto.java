28417ea1e475062ebb4ab416c7eca4c9771ec7b7
Davs <davidsilvateixeira5@hotmail.com>
2023-10-12 21:30:52 -0300

    feat: adiciona o método setNome

--- src/main/java/org/chorume/amarelinha/entities/Produto.java
+++ src/main/java/org/chorume/amarelinha/entities/Produto.java
@@ -1,4 +1,15 @@
 package org.chorume.amarelinha.entities;
 
 public class Produto {
+    private String nome;
+    private double peso; // Peso em kg
+
+    public Produto(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
