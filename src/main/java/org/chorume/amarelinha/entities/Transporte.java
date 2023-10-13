635e4868d3fe2def5b27ab6c79668dd5e38aed68
Davs <davidsilvateixeira5@hotmail.com>
2023-10-13 15:50:48 -0300

    feat: classe Transporte com construtor

--- src/main/java/org/chorume/amarelinha/entities/Transporte.java
+++ src/main/java/org/chorume/amarelinha/entities/Transporte.java
@@ -1,4 +1,14 @@
 package org.chorume.amarelinha.entities;
-
+import java.util.List;
 public class Transporte {
+    private List<Produto> produtos;
+    private List<Caminhao> caminhoes;
+
+    //Construtor
+    public Transporte(List<Produto> produtos, List<Caminhao> caminhoes) {
+        this.produtos = produtos;
+        this.caminhoes = caminhoes;
+    }
+
 }
+
