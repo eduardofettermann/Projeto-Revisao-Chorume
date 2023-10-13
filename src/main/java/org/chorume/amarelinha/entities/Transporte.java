c7d416c85e3d8b7c699ec88ce29aaf669e843a59
Davs <davidsilvateixeira5@hotmail.com>
2023-10-13 16:34:43 -0300

    feat: classe Transporte com método toString e esqueleto dos métodos de logistica

--- src/main/java/org/chorume/amarelinha/entities/Transporte.java
+++ src/main/java/org/chorume/amarelinha/entities/Transporte.java
@@ -26,5 +26,36 @@ public class Transporte {
     public void setCaminhoes(List<Caminhao> caminhoes) {
         this.caminhoes = caminhoes;
     }
+
+    //Aqui vai a lógica do transporte (incompleto ainda)
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public void adicionarCaminhao(Caminhao caminhao) {
+        caminhoes.add(caminhao);
+    }
+
+    public double calcularDistanciaTotal() {
+        // Lógica para calcular a distância total
+    }
+
+    public Caminhao determinarCaminhaoAdequado() {
+        // Lógica para determinar o caminhão adequado
+    }
+
+    public double calcularCustoTotal() {
+        // Lógica para calcular o custo total
+    }
+
+    //método toString da classe
+    @Override
+    public String toString() {
+        return "Transporte{" +
+                "produtos=" + produtos +
+                ", caminhoes=" + caminhoes +
+                '}';
+    }
+
 }
 
