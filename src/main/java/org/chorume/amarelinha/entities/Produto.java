b4d399c5d8ffd3a2cc2650df73385e8e4dc4bc4c
Davs <davidsilvateixeira5@hotmail.com>
2023-10-12 21:31:51 -0300

    feat: adiciona o método getNome

--- src/main/java/org/chorume/amarelinha/entities/Produto.java
+++ src/main/java/org/chorume/amarelinha/entities/Produto.java
@@ -9,6 +9,10 @@ public class Produto {
         this.peso = peso;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
