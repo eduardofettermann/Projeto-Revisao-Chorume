78970e0da9a6413c56ca13afc4f87aacb8759059
ebonatto <ebonatto.dev@gmail.com>
2023-10-16 12:15:34 -0300

    feat: iniciando Amarelinha.menu

--- src/main/java/org/chorume/amarelinha/Main.java
+++ src/main/java/org/chorume/amarelinha/Main.java
@@ -2,6 +2,6 @@ package org.chorume.amarelinha;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Amarelinha.menu();
     }
 }
